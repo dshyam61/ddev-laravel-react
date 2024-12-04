5e21efeb49781179ad7cec62e8854fbe660cbfac
shyambabu1989 <shyambabu1989@outlook.com>
2024-12-04 11:11:07 +0530

    nginx added

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+//use Illuminate\Support\Facades\Cache;
 
 Route::get('/', function () {
+    /* try {
+        Cache::set('shyam_mem', "dasari in memcache");
+        $val = Cache::get('shyam_mem');
+        dump($val);
+        dd(Cache::driver());
+    } catch (\Throwable $th) {
+        dd($th);
+        throw $th;
+    } */
     return view('welcome');
 });
